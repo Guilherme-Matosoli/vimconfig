b48129189a75337385ad666532a27248770d4dca
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-09-12 11:33:11 -0300

    :memo: docs(config/keymaps): Add comment to clarify the purpose of the keymap for 'go to definition'

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,4 +3,5 @@ vim.keymap.set("n", "<leader>xx", function()
   require("trouble").toggle()
 end)
 
+-- Go to definitionm, where this function is allocated
 vim.keymap.set('v', "<leader>d", "<cmd>lua vim.lsp.buf.definition()<CR>")
