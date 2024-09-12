b977ab413ba5fb64650b6915a1aae612dc29ef8e
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-09-12 11:38:20 -0300

    :pencil2: docs(config/keymaps): Update comment for clarity in `keymaps.lua`

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,5 @@ vim.keymap.set("n", "<leader>xx", function()
   require("trouble").toggle()
 end)
 
--- Go to definitionm, where this function is allocated
+-- Go to definition -> where the function under the cursor is allocated
 vim.keymap.set('v', "<leader>d", "<cmd>lua vim.lsp.buf.definition()<CR>")
