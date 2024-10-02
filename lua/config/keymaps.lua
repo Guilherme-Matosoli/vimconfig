f476646d58ee26d1360a59f7126e5335e525493e
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-10-02 08:46:01 -0300

    :sparkles: feat(config/keymaps): Add keymap for finding files by content using Telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,6 @@ end)
 
 -- Go to definition -> where the function under the cursor is allocated
 vim.keymap.set('v', "<leader>d", "<cmd>lua vim.lsp.buf.definition()<CR>")
+
+-- Find files by content
+vim.keymap.set('n', "<leader>fg", ":Telescope live_grep<CR>")
