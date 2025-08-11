8f3371b289eee5a7f05c32c4e9192e4ce07f69f3
Guilherme-Matosoli <guilhermematosoli@outlook.com>
2025-08-10 21:54:46 -0300

    configure keymaps to put selected texts inside characteres :wrench:

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,9 @@ vim.keymap.set('v', "<leader>d", "<cmd>lua vim.lsp.buf.definition()<CR>")
 
 -- Find files by content
 vim.keymap.set('n', "<leader>fp", ":Telescope live_grep<CR>", { desc = "Find files by content" })
+
+-- Set up select mode mappings
+vim.keymap.set('s', 'S', function()
+  vim.cmd('normal! gv')
+  vim.api.nvim_feedkeys('S', 'n', true)
+end, { desc = 'Surround in select mode' })
