dcdc428e7f770d7b6108f6c29ba3dee56a541dde
Guilherme-Matosoli <guilhermematosoli@outlook.com>
2025-08-13 15:12:02 -0300

    remove unsual keymaps :wrench:

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,12 +11,6 @@ vim.keymap.set('v', "<leader>d", "<cmd>lua vim.lsp.buf.definition()<CR>")
 -- Find files by content
 vim.keymap.set('n', "<leader>fp", ":Telescope live_grep<CR>", { desc = "Find files by content" })
 
--- Set up select mode mappings
-vim.keymap.set('s', 'S', function()
-  vim.cmd('normal! gv')
-  vim.api.nvim_feedkeys('S', 'n', true)
-end, { desc = 'Surround in select mode' })
-
 -- Rename declarations
 vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { desc = "Rename declarations" })
 
